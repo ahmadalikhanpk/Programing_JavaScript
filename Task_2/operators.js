adfe46ab1829a9f1677f6840479b3527ecf1084d
Ahmad Ali Khan <aaks2012pk@gmail.com>
2024-01-19 13:15:11 +0200

    add tasks

---
+++ Task_2/operators.js
@@ -0,0 +1,25 @@
+/* 
+- Create variables a and b, and assign them integer values.
+- Use add, subtraction, multiply, divide, and modulus operator for a and b. Store each result in separate variables (sum, difference, product, quotient, reminder).
+- Calculate the remainder of a divided by b using the modulus operator and store it in a variable called remainder.
+
+- Use console.log() to print the results of each operation to the console.
+
+- Add comments in your code to explain what each operation does. */
+let a = 123;
+let b = 321;
+add = a + b;
+subtraction = a - b;
+multiply = a * b;
+divide = a / b;
+modulus = a % b;
+/*"addition of a And b" */
+console.log( "addition of a And b", add);
+/*Subtraction of a And b*/
+console.log("subtraction of a And b", subtraction);
+/* multiplication of a And b */
+console.log("multiplication of a And b", multiply);
+/* dission of a And b*/
+console.log("dission of a And b", divide);
+/* module of a And b */
+console.log("module of a And b", modulus);

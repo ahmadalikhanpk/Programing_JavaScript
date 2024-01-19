0f95c7b1f502f56c34f5373ff9106f4ed8a9374d
Ahmad Ali Khan <aaks2012pk@gmail.com>
2024-01-19 12:55:39 +0200

    add tasks

---
+++ Task_2/data_types.js
@@ -0,0 +1,22 @@
+/*
+- Create a variable named age and assign it an integer value.
+- Create a variable named name and assign it a string value.
+- Create a variable named isStudent and assign it a boolean value.
+- Create a variable named hobbies and assign it an array containing a few string values (e.g., hobbies).
+- Create an info object with two properties: schoolName (a string) and grade (an integer).
+
+- Use console.log() to print each variable to the console.
+
+- Use the typeof operator to check the data type of each variable and print the result using console.log(). */
+var age = 21;
+var name = "ali";
+isstudent = true; 
+hobbies = ["walk", "drivr", "travel"];
+console.log(age);
+console.log(typeof age);
+console.log(name);
+console.log(typeof name);
+console.log(isstudent);
+console.log(typeof isstudent);
+console.log(hobbies);
+console.log(typeof hobbies);
